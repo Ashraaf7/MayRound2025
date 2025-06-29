ba4ec91ca9bac24806738a8aa34c257e0e04dc0b
Ashraaf7 <ahmedashraaf09@gmail.com>
2025-06-29 22:21:10 +0300

    adding logs

--- TestNG/src/test/java/LoginTest.java
+++ TestNG/src/test/java/LoginTest.java
@@ -14,17 +14,22 @@ public class LoginTest {
     @BeforeMethod(alwaysRun = true)
     public void setup()
     {
+        LogUtils.info("setup method is running");
         driver = new EdgeDriver();
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/login");
+        LogUtils.debug("Page Navigated to Login Page");
     }
 
     @Test(groups = {"valid","smoke"})
     public void validLoginTC()
     {
         driver.findElement(By.id("username")).sendKeys("tomsmith");
+        LogUtils.info("Username Entered");
         driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        LogUtils.info("Password Entered");
         driver.findElement(By.className("radius")).click();
+        LogUtils.info("Login Button Clicked");
         Assert.assertTrue(driver.findElement(By.id("flash")).isDisplayed(), "Login message is not appeared!");
         //throw  new RuntimeException(); to make the test fails
     }
